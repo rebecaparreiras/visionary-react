60bfbfb9ea960c0c17ccbdef66a6cc0487bfdcb0
Rebeca Parreiras <rebecaparreiras@hotmail.com>
2025-08-08 18:30:29 -0300

    ajuste: adição de host do Railway no Vite allowedHosts

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    host: true,
+    port: 4173,
+    allowedHosts: ['visionary-react-production.up.railway.app']
 });
