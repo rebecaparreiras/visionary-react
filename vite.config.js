cd6a70e2d9dbd118b384d2e699c6f2dbef46e796
Rebeca Parreiras <rebecaparreiras@hotmail.com>
2025-08-08 17:15:08 -0300

    atualizações de packages

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,10 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      swiper: path.resolve(__dirname, 'node_modules/swiper'),
+    },
+  },
+});
+
